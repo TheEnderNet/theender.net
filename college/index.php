eb8ff40a7c613cbd3ae558df2ea64c53c0f581ec
Josh <that.joshua.green@gmail.com>
2014-05-29 18:28:37 +0100

    last time before i give up

--- college/index.php
+++ college/index.php
@@ -14,15 +14,15 @@
         <div class="large-12 columns"> <h1>Recent additions</h1> </div>
         <div class="large-4 columns panel">
             <h2 style="color:blue">PlayStation</h2>
-            <p>This is the start of a paragraph for teh PlayStation content</p>
+            <p>This is the start of a paragraph for the PlayStation content</p>
         </div>
         <div class="large-4 columns panel">
             <h2 style="color:green">Xbox</h2>
-            <p>This is the start of a paragraph for teh Xbox content</p>
+            <p>This is the start of a paragraph for the Xbox content</p>
         </div>
         <div class="large-4 columns panel">
             <h2 style="color:red">PC</h2>
-            <p>This is the start of a paragraph for teh PC content</p>
+            <p>This is the start of a paragraph for the PC content</p>
         </div>
     </div>
 
