87a246283a985714062564759e420548a441ab5d
Josh Ender <that.joshua.green@gmail.com>
2014-06-07 21:31:55 +0100

    meh

--- theenderweb/story/index.php
+++ theenderweb/story/index.php
@@ -57,9 +57,9 @@
         <p>
           *loud gasp*<br>
           I awake suddenly, body aching, in what appears to be a circular road. Completely disoriented I get myself to my feet and begin to wander to the sidewalk.
-          I look around; all I can see is dimly lit bits of what looks like a small residential district. I look up to see the sky; it's a full moon tonight.
+          I look around; all I can see is dimly lit bits of what looks like a small residential district. I look up to see the sky; it's a full moon tonight.<br>
           "That's the last time I use that blasted vortex manipulator!" I say to myself. "I can't keep dying when it's not necessary...."
-          As I rest myself against a light post, I check my heartbeat locator and see a blip. "There he is...” I say to myself as I walk off in the direction of the blip.
+          As I rest myself against a street light, I check my heartbeat locator and see a blip. "There he is...” I say to myself as I walk off in the direction of the blip.
         </p>
       </div>
     </div>
