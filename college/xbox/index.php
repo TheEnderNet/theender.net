a0ac5dec69464adc5589fa0d4286218f64a083e2
Josh <JoshTheEnder@users.noreply.github.com>
2014-06-12 12:18:16 +0100

    Update index.php

--- college/xbox/index.php
+++ college/xbox/index.php
@@ -11,18 +11,18 @@
   <body>
     <?php include "../resources/bannerAndNavbar.php"; ?>
     <div class="row">
-        <div class="large-12 columns"> <h1>Recent additions</h1> </div>
-        <div class="large-4 columns panel">
-            <h2 style="color:blue">PlayStation</h2>
+        <div class="large-12 columns"> <h1><center> Recent additions </center></h1></div>
+        <div class="large-6 columns panel">
+            <a href="#360"><h2 style="color:blue"> Xbox 360 </h2></a>
+            <hr>
+            <img src="../img/noimg.png">
             <p>This is the start of a paragraph for teh PlayStation content</p>
         </div>
-        <div class="large-4 columns panel">
-            <h2 style="color:green">Xbox</h2>
+        <div class="large-6 columns panel">
+            <a href="#one"><h2 style="color:green"> Xbox One </h2></a>
+            <hr>
+            <img src="../img/noimg.png">
             <p>This is the start of a paragraph for teh Xbox content</p>
-        </div>
-        <div class="large-4 columns panel">
-            <h2 style="color:red">PC</h2>
-            <p>This is the start of a paragraph for teh PC content</p>
         </div>
     </div>
 
