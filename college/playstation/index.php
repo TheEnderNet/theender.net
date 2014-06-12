ec20e3af2292ad3dc90dbf39f70035c67f6756cf
Josh <JoshTheEnder@users.noreply.github.com>
2014-06-12 11:19:09 +0100

    Update index.php

--- college/playstation/index.php
+++ college/playstation/index.php
@@ -14,21 +14,21 @@
         <div class="row"><!-- Recent additions section -->
             <div class="large-12 columns"> <h1><center> Recent additions </center></h1> </div><hr>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <a href="#2"><h2 style="color:blue"> PlayStation 2 </h2></a>
                 <hr>
                 <img src="img/dah2.jpg" width="128" height="163" alt="Errm, crap">
                 <p>This is the start of a paragraph for the PlayStation 2 content</p>
             </div>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <a href="#3"><h2 style="color:green"> PlayStation 3 </h2></a>
                 <hr>
                 <img src="img/gta5.jpg" width="128" height="163" alt="Errm, crap">
                 <p>This is the start of a paragraph for the Playstation 3 content</p>
             </div>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <a href="#4"><h2 style="color:red">Playstation 4 </h2></a>
                 <hr>
                 <p>This is the start of a paragraph for the Playstation 4 content</p>
@@ -41,21 +41,21 @@
             <a name="2">
             <div class="large-12-centered columns"> <h1><center> Playstation 2 </center></h1> </div><hr>
             </a>
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <h2> Destroy All Humans 2 </h2>
                 <hr>
                 <img src="img/dah2.jpg" width="128" height="163">
                 <p>This is the best game in the world</p>
             </div>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <h2> Game title here</h2>
                 <hr>
                 <img src="../img/noimg.png">
                 <p>This is the best game in the world</p>
             </div>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <h2> Game title here</h2>
                 <hr>
                 <img src="../img/noimg.png">
@@ -70,7 +70,7 @@
             <div class="large-12-centered columns"> <h1><center> Playstation 3 </center></h1> </div><hr>
             </a>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <h2>Grand Theft Auto 5</h2>
                 <hr>
                 <img src="img/gta5.jpg" width="150" height="183">
@@ -79,14 +79,14 @@
                 </p>
             </div>
 
-            <div class="large-4 columns panel">
-                <h2>Little Bit Planet 2</h2>
+            <div class="large-4 columns panel" data-equalizer-watch>
+                <h2>Little Big Planet 2</h2>
                 <hr>
                 <img src="http://upload.wikimedia.org/wikipedia/en/e/e7/Littlebigplanet2-boxart.jpg" width="150" height="183"><a href="#" class="button tiny">&#8356;25</a>
                 <p>This is the best game in the world</p>
             </div>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <h2> Game title here</h2>
                 <hr>
                 <img src="../img/noimg.png">
@@ -100,21 +100,21 @@
             <div class="large-12-centered columns"> <h1><center> Playstation 4 </center></h1> </div><hr>
             </a>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <h2>Assassin's Creed Unity</h2>
                 <hr>
                 <img src="img/ACUnity.png" width="128" height="163"><a href="#" class="button tiny">&#8356;40</a>
                 <p>This is the best game in the world</p>
             </div>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <h2> Game title here</h2>
                 <hr>
                 <img src="../img/noimg.png">
                 <p>This is the best game in the world</p>
             </div>
 
-            <div class="large-4 columns panel">
+            <div class="large-4 columns panel" data-equalizer-watch>
                 <h2> Game title here</h2>
                 <hr>
                 <img src="../img/noimg.png">
