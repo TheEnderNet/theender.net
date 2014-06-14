9393d7a0e3887318c7f873fb990fd99d880c7ad7
Josh <that.joshua.green@gmail.com>
2014-06-14 01:23:59 +0100

    derp

--- theenderweb/_resources/header.php
+++ theenderweb/_resources/header.php
@@ -2,6 +2,6 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $Title ?></title>
-    <link rel="stylesheet" href="/_resources/css/foundation.css" />
-    <script src="/_resources/js/modernizr.js"></script>
+    <link rel="stylesheet" href="../_resources/css/foundation.css" />
+    <script src="../_resources/js/modernizr.js"></script>
   </head>
