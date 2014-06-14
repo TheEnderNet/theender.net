9920ac9fd2e7fe7f57698cb43d9713bb63064727
Josh <that.joshua.green@gmail.com>
2014-06-14 01:57:05 +0100

    hetrt

--- theenderweb/story/index.php
+++ theenderweb/story/index.php
@@ -24,17 +24,15 @@
     </div>
     <br>
     <div class="row">
-      <div class="panel" style="padding-top: 20">
-        <h1>Prologue</h1>
-        <hr>
-        <p>
-          <b>*loud gasp*</b><br>
-          I awake suddenly, body aching, in what appears to be a circular road. Completely disoriented I get myself to my feet and begin to wander to the sidewalk. <br>
-          I look around; all I can see is dimly lit bits of what looks like a small residential district. I look up to see the sky; it's a full moon tonight. <br>
-          “Urghh, that wasn’t the experience I had expected” I say to myself. <br>
-          As I rest myself against a light post, I check my heartbeat locator and see a blip. "There he is...” I say to myself as I walk off in the direction of the blip. <br>
-        </p>
-      </div>
+      <h1 style="color:white">Prologue</h1>
+      <hr>
+      <p style="color:white">
+        <b>*loud gasp*</b><br>
+        I awake suddenly, body aching, in what appears to be a circular road. Completely disoriented I get myself to my feet and begin to wander to the sidewalk. <br>
+        I look around; all I can see is dimly lit bits of what looks like a small residential district. I look up to see the sky; it's a full moon tonight. <br>
+        “Urghh, that wasn’t the experience I had expected” I say to myself. <br>
+        As I rest myself against a light post, I check my heartbeat locator and see a blip. "There he is...” I say to myself as I walk off in the direction of the blip. <br>
+      </p>
     </div>
     
     <script src="../_resources/js/vendor/jquery.js"></script>
