a0587f5b3f330bc253c6b7234c8549e29d6cbd62
Josh <JoshTheEnder@users.noreply.github.com>
2014-06-19 14:24:12 +0100

    Update index.php

--- college/index.php
+++ college/index.php
@@ -13,16 +13,25 @@
     <div class="row">
         <div class="large-12 columns"> <h1>Recent additions</h1> </div>
         <div class="large-4 columns panel">
-            <h2 style="color:blue">PlayStation</h2>
-            <p>This is the start of a paragraph for the PlayStation content</p>
+            <h2 style="color:blue">Playstation</h2>
+            <h3>Grand Theft Auto 5</h3>
+            <hr>
+            <img src="img/gta5.jpg" width="150" height="183">
+            <p>This is the best game in the world</p>
         </div>
         <div class="large-4 columns panel">
             <h2 style="color:green">Xbox</h2>
-            <p>This is the start of a paragraph for the Xbox content </p>
+            <h3>game title here</h3>
+            <hr>
+            <img src="img/noimg.png">
+            <p>Game description here</p>
         </div>
         <div class="large-4 columns panel">
             <h2 style="color:red">PC</h2>
-            <p>This is the start of a paragraph for the PC content</p>
+            <h3>game title here</h3>
+            <hr>
+            <img src="img/noimg.png">
+            <p>Game description here</p>
         </div>
     </div>
 
