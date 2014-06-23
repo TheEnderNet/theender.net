8828229fb03034cf2d6eba153196dce3c5df0940
Josh Ender <that.joshua.green@gmail.com>
2014-06-23 22:08:56 +0100

    Added the start of the LaserCraft section

---
+++ theenderweb/lasercraft/index.php
@@ -0,0 +1,32 @@
+<?php
+  ini_set('display_errors','On');
+  $Title = "LaserCraft | Ender";
+  $PageName = "LaserCraft";
+  ini_set("log_errors", 1);
+  ini_set("error_log", "./php-error.log");
+?>
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+
+  <?php include"../_resources/header.php"; ?>
+
+  <body style="background-color:black;background-image:url('../_resources/img/bg.png');background-repeat:no-repeat;background-position:center top;">
+    
+    <?php include "../_resources/navbar.php"; ?>
+    <div class="row">
+      <h1 style="color:white"><i>Well you found me, was it woth it?</i></h1>
+      <hr>
+      <p style="color:white">
+        hjsj skjdh dsa asdkjsadhasdkhasd hksda hksda hasd
+      </p>
+    </div>
+    <br>
+
+       
+    <script src="../_resources/js/vendor/jquery.js"></script>
+    <script src="../_resources/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
