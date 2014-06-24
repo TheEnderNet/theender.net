2791aa2408681a3676e6ec5aa94bbcaae417b76b
Josh <that.joshua.green@gmail.com>
2014-06-24 18:00:51 +0100

    added basic info and irc link

--- theenderweb/lasercraft/index.php
+++ theenderweb/lasercraft/index.php
@@ -14,14 +14,18 @@
     
     <?php include "../_resources/navbar.php"; ?>
     <div class="row">
-      <h1 style="color:white"><i>Well you found me, was it woth it?</i></h1>
+      <h1 style="color:white"><center>Well, what <i>is</i> LaserCraft?</center></h1>
       <hr>
       <p style="color:white">
-        hjsj skjdh dsa asdkjsadhasdkhasd hksda hksda hasd
+        LaserCraft is a modification for Minecraft made by me (JoshTheEnder).<br>
+        It adds, as the name suggests, lasers to Minecraft. This page (and other pages along this path) will have information about the mod.<br>
+        For now it'll be an empty shell until the first initial release :)<br>
+        If you want to come chat with me and see the progress I make, come on the IRC channel 
+        <a href="http://webchat.esper.net/?nick=SiteUser..&channels=LaserCraft&prompt=1">#LaserCraft @ irc.esper.net</a>
+        (that is a link to the esper webchat, if you have a desktop client and are connected to esper just do /join #LaserCraft )
       </p>
     </div>
     <br>
-
        
     <script src="../_resources/js/vendor/jquery.js"></script>
     <script src="../_resources/js/foundation.min.js"></script>
