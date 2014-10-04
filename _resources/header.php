ba3caa3ba0850b953cad0598a29a7d234d44d7e6
Josh <that.joshua.green@gmail.com>
2014-10-04 12:33:25 +0100

    hmmm

--- _resources/header.php
+++ _resources/header.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="icon" type="image/png" href="/favico.png">
+    <link rel="icon" type="image/png" href="../favico.png">
     <title><?php echo $Title ?></title>
     <link rel="stylesheet" href="../_resources/css/foundation.css" />
     <script src="../_resources/js/modernizr.js"></script>
