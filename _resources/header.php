df69d2bfe4ebf97e87e08a400bc3acc167d9d254
Josh <JoshTheEnder@users.noreply.github.com>
2014-12-09 16:20:02 +0000

    Update header.php

--- _resources/header.php
+++ _resources/header.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="./favico.png">
     <title><?php echo $Title ?></title>
     <link rel="stylesheet" type="text/css" href="../_resources/css/foundation.css">
