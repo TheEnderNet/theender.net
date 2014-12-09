5021cf3465cbe6aa9264f24dc7729bc1e48c92c5
Josh <JoshTheEnder@users.noreply.github.com>
2014-12-09 16:12:41 +0000

    Update header.php

--- _resources/header.php
+++ _resources/header.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/png" href="./favico.png">
     <title><?php echo $Title ?></title>
-    <link rel="stylesheet" type="text/css" href="./_resources/css/foundation.css" />
-    <script src="./_resources/js/modernizr.js"></script>
+    <link rel="stylesheet" type="text/css" href="../_resources/css/foundation.css">
+    <script src="../_resources/js/modernizr.js"></script>
     <link href='http://fonts.googleapis.com/css?family=Audiowide|Rock+Salt|Inconsolata' rel='stylesheet' type='text/css'>
   </head>
