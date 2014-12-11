837ecb323c0594b820224e18c3b91bee3d1a24f2
Josh <JoshTheEnder@users.noreply.github.com>
2014-12-11 13:09:56 +0000

    Update index.php

--- story/index.php
+++ story/index.php
@@ -46,9 +46,9 @@
       </ul>
     </div>
     
-    <script src="../_resources/js/vendor/jquery.js"></script>
-    <script src="_resources/js/foundation/foundation.js"></script>
-    <script src="_resources/js/foundation/foundation.topbar.js"></script>
+    <script src="/_resources/js/vendor/jquery.js"></script>
+    <script src="/_resources/js/vendor/fastclick.js"></script>
+    <script src="/_resources/js/foundation.min.js"></script>
     <script>
       $(document).foundation({
           orbit: {
