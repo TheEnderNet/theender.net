25a6e7fa5436b0d0b831a24aaa92401f967e8393
Ender <that.joshua.green@gmail.com>
2014-12-15 13:11:00 +0000

    updating stuffs

--- _resources/navbar.php
+++ _resources/navbar.php
@@ -5,53 +5,50 @@
     </li>
     <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
   </ul>
+    <section class="top-bar-section">
+        <!-- Right Nav Section -->
+        <ul class="right">
+            <?php if ($_SERVER['PHP_SELF'] !== "/index.php") {
+                echo "<li><a href=\"/\" style=\"color:green\">Back Home</a></li>";
+                }
+            ?>
+            <li class="has-dropdown">
+                <a href="/information/">Informational Stuff</a>
+                <ul class="dropdown">
+                    <li><a href="/information/#about">Who the hell am I?</a></li>
+                    <li><a href="/information/#contact">How can I contact you?</a></li>
+                </ul>
+            </li>
+      
+            <li class="has-dropdown">
+                <a href="#">Other Sites</a>
+                <ul class="dropdown">
+                    <li class="active"><a href="http://www.theender.net">EnderNet</a></li>
+                    <li><a href="http://stargatetech.theender.net/">StargateTech2 Wiki</a></li>
+                    <li><a href="http://stats.theender.net">Ender's IRC Statistics</a></li>
+                    <li><a href="http://www.theender.net/jenkins">Ender's Jenkins CI</a></li>
+                </ul>
+            </li>
+        </ul>
     
-    <div class="show-for-large-up">
-        <section class="top-bar-section">
-            <!-- Right Nav Section -->
-            <ul class="right">
-                <?php if ($_SERVER['PHP_SELF'] !== "/index.php") {
-                    echo "<li><a href=\"/\" style=\"color:green\">Back Home</a></li>";
-                    }
-                ?>
-                <li class="has-dropdown">
-                    <a href="/information/">Informational Stuff</a>
-                    <ul class="dropdown">
-                        <li><a href="/information/#about">Who the hell am I?</a></li>
-                        <li><a href="/information/#contact">How can I contact you?</a></li>
-                    </ul>
-                </li>
-          
-                <li class="has-dropdown">
-                    <a href="#">Other Sites</a>
-                    <ul class="dropdown">
-                        <li class="active"><a href="http://www.theender.net">EnderNet</a></li>
-                        <li><a href="http://stargatetech.theender.net/">StargateTech2 Wiki</a></li>
-                        <li><a href="http://stats.theender.net">Ender's IRC Statistics</a></li>
-                        <li><a href="http://www.theender.net/jenkins">Ender's Jenkins CI</a></li>
-                    </ul>
-                </li>
-            </ul>
-    
-        <!-- Left Nav Sesction -->
-            <ul class="left">
-                <li class="has-dropdown">
-                    <a href="/projects/">Projects</a>
-                    <ul class="dropdown">
-                        <li><a href="/projects/#LaserCraft">Laser Craft</a></li>
-                        <li><a href="/projects/#EnderBot">EnderBot</a></li>
-                    </ul>
-                </li>
-                <li><a href="/story/">Story Corner</a></li>
-                <li class="has-dropdown">
-                    <a href="#">Mods</a>
-                    <ul class="dropdown">
-                        <li><a href="/lasercraft/">LaserCraft</a></li>
-                    </ul>
-                </li>
-            </ul>
-        </section>
-    </div>
+    <!-- Left Nav Sesction -->
+        <ul class="left">
+            <li class="has-dropdown">
+                <a href="/projects/">Projects</a>
+                <ul class="dropdown">
+                    <li><a href="/projects/#LaserCraft">Laser Craft</a></li>
+                    <li><a href="/projects/#EnderBot">EnderBot</a></li>
+                </ul>
+            </li>
+            <li><a href="/story/">Story Corner</a></li>
+            <li class="has-dropdown">
+                <a href="#">Mods</a>
+                <ul class="dropdown">
+                    <li><a href="/lasercraft/">LaserCraft</a></li>
+                </ul>
+            </li>
+        </ul>
+    </section>
 </nav>
 
 
