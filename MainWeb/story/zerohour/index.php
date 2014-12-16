65c3f4cb515bbe8ee1fe6c8f49ddda17ba137405
Ender <that.joshua.green@gmail.com>
2014-12-16 14:23:29 +0000

    making stuff shorter ready for static GET requesting

---
+++ MainWeb/story/zerohour/index.php
@@ -0,0 +1,33 @@
+<?php
+  ini_set('display_errors','On');
+  $Title = "Ender's Story Corner";
+  $PageName = "Ender's Story Corner";
+  ini_set("log_errors", 1);
+  ini_set("error_log", "./php-error.log");
+?>
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+
+  <?php include $_SERVER['DOCUMENT_ROOT']."/_resources/header.php"; ?>
+
+  <body style="background-color:black;background-image:url('/_resources/img/bg.png');background-repeat:no-repeat;background-position:center top;">
+    
+    <?php include $_SERVER['DOCUMENT_ROOT']."/_resources/navbar.php"; ?>
+    <div class="row"><h1 style="color:white;font-family:'Audiowide';text-align:center">Zero Hour</h1></div>
+    <div class="row">
+      <hr>
+      <p style="color:white;text-align:center">
+        <img src="http://imgs.xkcd.com/comics/joshing.png">
+      </p>
+    </div>
+
+
+    <script src="/_resources/js/vendor/jquery.js"></script>
+    <script src="/_resources/js/vendor/fastclick.js"></script>
+    <script src="/_resources/js/foundation.min.js"></script>
+    
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
