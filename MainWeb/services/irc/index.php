a92d541e37b267a1bf3031fc35aa3af3f4da84ef
Ender <that.joshua.green@gmail.com>
2015-01-18 17:29:52 +0000

    added the start of the services section, first up is IRC :D

---
+++ MainWeb/services/irc/index.php
@@ -0,0 +1,21 @@
+<?php
+  ini_set('display_errors','On');
+  $Title = "Ender's Services";
+  $PageName = "Ender's Services";
+?>
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+  
+  <?php include $_SERVER['DOCUMENT_ROOT']."_resources/header.php"; ?>
+  
+  <body style="background-color:black;background-image:url('../_resources/img/bg.png');background-repeat:no-repeat;background-position:center top;">
+    <?php include $_SERVER['DOCUMENT_ROOT']."_resources/navbar.php"; ?>
+    
+    <script src="/_resources/js/vendor/jquery.js"></script>
+    <script src="/_resources/js/vendor/fastclick.js"></script>
+    <script src="/_resources/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
