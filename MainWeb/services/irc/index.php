058afa6de51a3021bae8d3d6feb2e6a0b2bbb857
Ender <that.joshua.green@gmail.com>
2015-01-19 14:18:30 +0000

    whoops

--- MainWeb/services/irc/index.php
+++ MainWeb/services/irc/index.php
@@ -11,7 +11,7 @@
   <body style="background-color:black;background-image:url('../_resources/img/bg.png');background-repeat:no-repeat;background-position:center top;">
     <?php include $_SERVER['DOCUMENT_ROOT']."/_resources/navbar.php"; ?>
     <dib class="row">
-        <div class="large-6-centered columns">
+        <div class="large-6-centered columns panel">
             <p style="text-shadow: 2px 2px 8px #FFFFFF;text-align:center">
                 This page is still a WIP
             </p>
