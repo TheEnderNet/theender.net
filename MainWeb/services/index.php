2d12fe53d9a6717050932108883a36c52e6c5d80
Ender <that.joshua.green@gmail.com>
2015-01-19 15:01:56 +0000

    testing of stuff

---
+++ MainWeb/services/index.php
@@ -0,0 +1,44 @@
+<?php
+  ini_set('display_errors','On');
+  $Title = "Ender's Services";
+  $PageName = "Ender's Services";
+?>
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+  
+  <?php include $_SERVER['DOCUMENT_ROOT']."/_resources/header.php"; ?>
+  
+  <!--<body style="background-color:black;background-image:url('/_resources/img/bg.png');background-repeat:no-repeat;background-position:center top;">-->
+  <body>
+    <?php include $_SERVER['DOCUMENT_ROOT']."/_resources/navbar.php"; ?>
+    <div class="row">
+        <div class="large-6-centered columns">
+            <h1 style="text-shadow: 0 0 8px #FFFFFF;text-align:center">
+                This section is still a WIP
+            </h1>
+        </div>
+    </div>
+    <div class="row">
+        <?php
+            echo $_SERVER['SCRIPT_FILENAME'];
+        ?>
+    
+    
+    <?php
+      $files = array_diff(scandir($dir), array('..', '.'));
+      foreach ($files as $file) {
+        $myfile = fopen($dir."/".$file, "r") or die("Unable to open file!");
+        echo fread($myfile,filesize($dir."/".$file));
+        fclose($myfile);
+      };
+    ?>
+    
+    </div>
+    <script src="/_resources/js/vendor/jquery.js"></script>
+    <script src="/_resources/js/vendor/fastclick.js"></script>
+    <script src="/_resources/js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
