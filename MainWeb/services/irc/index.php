5a8a8438dc1d13d070d39a9accfe241430873f73
Ender <that.joshua.green@gmail.com>
2015-01-19 14:26:15 +0000

    fricken dib -_-

--- MainWeb/services/irc/index.php
+++ MainWeb/services/irc/index.php
@@ -12,7 +12,7 @@
     <?php include $_SERVER['DOCUMENT_ROOT']."/_resources/navbar.php"; ?>
     <div class="row">
         <div class="large-6-centered columns">
-            <h1 style="text-shadow: 0 0 3px #FFFFFF;text-align:center">
+            <h1 style="text-shadow: 0 0 8px #FFFFFF;text-align:center">
                 This page is still a WIP
             </h1>
         </div>
