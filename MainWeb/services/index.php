d581ef4d953adaf8dfe4a55be6fbeab21714bf1c
Ender <that.joshua.green@gmail.com>
2015-01-20 08:29:15 +0000

    Start of the services page, still dithering how i want to lay it out

--- MainWeb/services/index.php
+++ MainWeb/services/index.php
@@ -3,38 +3,32 @@
   $Title = "Ender's Services";
   $PageName = "Ender's Services";
 ?>
+
 <!DOCTYPE HTML>
 <html class="no-js" lang="en">
-  
+
   <?php include $_SERVER['DOCUMENT_ROOT']."/_resources/header.php"; ?>
-  
+
   <!--<body style="background-color:black;background-image:url('/_resources/img/bg.png');background-repeat:no-repeat;background-position:center top;">-->
   <body>
     <?php include $_SERVER['DOCUMENT_ROOT']."/_resources/navbar.php"; ?>
     <div class="row">
-        <div class="large-6-centered columns">
-            <h1 style="text-shadow: 0 0 8px #FF00FF;text-align:center">
-                This section is still a WIP
-            </h1>
-        </div>
+      <div class="large-6-centered columns">
+        <h1 style="text-shadow: 0 0 8px #FF00FF;text-align:center">
+          This section is still a WIP, also I didn't know css could do text effects
+        </h1>
+      </div>
     </div>
+    
     <div class="row">
-        <?php
-            echo getcwd();
-        ?>
-    
-    
-    <?php
-      $files = array_diff(scandir(getcwd()), array('..', '.'));
-      echo $files;
-      foreach ($files as $file) {
-        $myfile = fopen($dir."/".$file, "r") or die("Unable to open file!");
-        echo fread($myfile,filesize($dir."/".$file));
-        fclose($myfile);
-      };
-    ?>
-    
+      <div class="small-6 columns panel">
+        
+      </div>
+      <div class="small-6 columns panel">
+        
+      </div>
     </div>
+
     <script src="/_resources/js/vendor/jquery.js"></script>
     <script src="/_resources/js/vendor/fastclick.js"></script>
     <script src="/_resources/js/foundation.min.js"></script>
