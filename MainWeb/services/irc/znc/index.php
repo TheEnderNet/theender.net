d126260e9821985dc5fe44d89ba3f615aad53c51
LizzyTrickster <LizzyTrickster@users.noreply.github.com>
2015-06-04 12:36:49 +0100

    Added BNC T&C, need to redo my whole site soon and make it a bit better

--- MainWeb/services/irc/znc/index.php
+++ MainWeb/services/irc/znc/index.php
@@ -13,12 +13,38 @@
           <b style="font-family:'Audiowide';color:white">Ender/Lizzy's Bouncer</b>
         </h1>
         <h3 style="<?php echo $Color; ?>">
-          I run an IRC bouncer on my 3 servers, Heyo, Anquietas and Thor. Most of the current users are
-          primararilly on the EsperNet network but there are no limitations on what network you can join
-          using my bouncer. If you would like to request an account please message me on 
-          <a href="/services/irc/network/">my network</a> as <b>Lizzy</b>.
+          I run an IRC bouncer. If you would like to request an account please message me on 
+          <a href="/services/irc/network/">my network</a> as <b>Lizzy</b> or fill out
+          <a href="">this form</a>.<br><br>
+          <a href="" data-reveal-id="modModal">Terms &amp; Conditions for bouncer use.</a>
         </h3>
       </div>
+    </div>
+
+    <div id="modModal" class="reveal-modal" data-reveal>
+      <div class="row">
+        <h2 style="text-align:center">Terms and Conditions for EnderBNC</h2><hr>
+        <p>
+          By using this BNC ( now refered to as "service") you hereby agree to adhere to the points lited below<br>
+          <ol>
+            <li>You may not use the service to:<br>
+              <ul>
+                <li>You may not use the service to evade bans.</li>
+                <li>You may not use the service to promote or spread hate speach/racism</li>
+              </ul>
+            </li>
+            <li>You may use the service to:<br>
+              <ul>
+                <li>Have fun</li>
+                <li>Chat with your friends</li>
+                <li>Other things that don't involve the stuff on the list above</li>
+              </ul>
+            </li>
+            <li>Remember to use common sense! If you feel something may offend someone, keep it to yourself.</li>
+          </ol>
+        </p>
+      </div>
+      
     </div>
 
     <script src="/_resources/js/vendor/jquery.js"></script>
