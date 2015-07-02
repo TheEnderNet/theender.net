928a4a01a01a80de1739cccb2e4dcd7df3a1925f
Ender <ender@NetTechJ.seec.local>
2015-07-02 15:48:47 +0100

    derpdedep

--- MainWeb/_resources/header.php
+++ MainWeb/_resources/header.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" type="image/png" href="/favico.png">
-  <title><?php echo $Title ?></title>
+  <title><?php echo str_replace("Ender", "Lizzy", $Title); ?></title>
   <link rel="stylesheet" type="text/css" href="/_resources/css/foundation.css">
   <link rel="stylesheet" type="text/css" href="/_resources/css/foundation-icons.css">
   <script type="text/javascript" src="/_resources/js/modernizr.js"></script>
