7acb73c1c1bb4ea8f5cd8ce9d131edbfcc05a57a
Lizzy Trickster <lizzy.trickster@outlook.com>
2017-03-29 11:14:37 +0100

    Squashed commit of the following:
    commit cd78c422f32554711e76f9ca6b0f83e743d6c8ef
    Author: Lizzy Trickster <lizzy.trickster@outlook.com>
    Date:   Wed Mar 29 11:09:57 2017 +0100
    
        some other files, IDFK do i?
    
    commit 708f7011cc778626a8058929435cdaa923e3548e
    Author: Lizzy Trickster <lizzy.trickster@outlook.com>
    Date:   Thu Sep 22 14:44:40 2016 +0100
    
        Code stuffs, still in-dev atm
    
    commit ad86b7a97e8c42aecb587559d6f401bbee1c55a6
    Author: Lizzy Trickster <lizzy.trickster@outlook.com>
    Date:   Thu Sep 22 14:39:45 2016 +0100
    
        Readme
    
    commit 72e889b2f4cc06571e18c01dcf8611bde6db9025
    Author: Lizzy Trickster <lizzy.trickster@outlook.com>
    Date:   Thu Sep 22 12:22:18 2016 +0100
    
        ignoring this crap
    
    commit 6f4a8ab0f8bd5194b5dbca2929b3d9ebc3c65809
    Author: Lizzy Trickster <lizzy.trickster@outlook.com>
    Date:   Thu Sep 22 12:18:44 2016 +0100
    
        Added a .gitignore
    
        ignores venv stuff and Vim swap files

---
+++ README.md
@@ -0,0 +1,6 @@
+EnderNet Websitery
+==========
+
+
+Coded in Python and powered by Flask.
+More to come in this file later
---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+#!flask/bin/python
+import os
+from flask import Flask
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import basedir
+
+app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID( app, os.path.join(basedir, 'tmp') )
+
+from app import views, models
